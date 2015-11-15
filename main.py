4c65a039566453a0f79ed42ba59661dc23ee440e
Becca Petrin <beccapetrin@gmail.com>
2015-11-15 14:05:17 -0800

    sample config

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+# TODO instructons on how to run this script
+# including how to install Python
+# and on Github
+
+def main():
+
+	linkedInUsername = ""
+	linkedInPassword = ""
+
+	# enter LinkedIn search terms at the command line
+
+	# get the main search results page(s)
+
+	# scrape email addresses
+
+	# stick into a CSV file
+
+	
+
+if __name__ == "__main__":
+	main()
